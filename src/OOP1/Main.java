0209e9bd3829a26fa8b93314e10483b7c2bf3a51
deanvt1 <1219213@student.roc-nijmegen.nl>
2026-03-13 09:17:16 +0100

    variabelen ovchipkaart

--- src/OOP1/Main.java
+++ src/OOP1/Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < ; i++)
-        {
 
-        } for (int i = 0; i <; i++)
-        {
-
-        }
-    }
-}
 
 
 
--- src/OVchipkaart.java
+++ src/OVchipkaart.java
@@ -1,3 +1,6 @@
 public class OVchipkaart
-{
+{   //variabelen
+    private int kaartNummer;
+    private double saldo;
+    private boolean geldig;
 }
