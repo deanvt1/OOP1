08e97d406c770ace50548746ca96ae67106474b2
deanvt1 <1219213@student.roc-nijmegen.nl>
2026-04-17 10:43:30 +0200

    } stoppen

--- src/OOP1/opwaarderen.java
+++ src/OOP1/opwaarderen.java
@@ -4,32 +4,39 @@ public class opwaarderen
 { //variabelen
     private OVchipkaart actieveKaart;
 
-//constructor
-    public void scanKaart(OVchipkaart kaart) {
-      this.actieveKaart = kaart;
+    //constructor
+    public void scanKaart(OVchipkaart kaart)
+    {
+        this.actieveKaart = kaart;
     }
 
-    public void opwaarderen(double bedrag) {
-       if (actieveKaart == null) {
-        System.out.println ("fout");
-        } else {
-        actieveKaart.verhoogSaldo(bedrag);
-    }
+    public void opwaarderen(double bedrag)
+    {
+        if (actieveKaart == null)
+        {
+            System.out.println("fout");
+        }
+        else
+        {
+            actieveKaart.verhoogSaldo(bedrag);
+        }
     }
 
     public void aanvullenTot(double doelBedrag)
     {
-        if (actieveKaart == null) {
-        System.out.println("fout");
-    } else {
-    double verschil = doelBedrag - actieveKaart.getSaldo();
-    actieveKaart.verhoogSaldo(verschil);
+        if (actieveKaart == null)
+        {
+            System.out.println("fout");
+        }
+        else
+        {
+            double verschil = doelBedrag - actieveKaart.getSaldo();
+            actieveKaart.verhoogSaldo(verschil);
+        }
+    }
 
-
-
-    public void stoppen() {
-actieveKaart = null;
+    public void stoppen()
+    {
+        actieveKaart = null;
     }
 }
-
-//functie
